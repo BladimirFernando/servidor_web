a22e353dfdca9e67132d952aaebcbea146008c90
bladimirh179@gmail.com <bladimirh179@gmail.com>
2023-08-17 09:30:36 -0600

    arrancamos el servidor

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const port = 5000
+
+const server = http.createServer((req, res) => {
+    res.end('Hola alumnos')
+})
+
+
+server.listen(port, () =>{
+    console.log('Arranca el servidor')
+})
